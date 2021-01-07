9c55678753beacd33c3e0e17c42ed7db1a636030
Jefferson William Chen <jeffwilliamchen@ieee.org>
2021-01-07 12:08:51 -0800

    added users display

--- React/guided-projects/github-finder/src/App.js
+++ React/guided-projects/github-finder/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Navbar />
+        <Navbar icon={faGithub} title={'GitHub Finder'} />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
--- React/guided-projects/github-finder/src/components/layout/Navbar.js
+++ React/guided-projects/github-finder/src/components/layout/Navbar.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
+  static defaultProps = {
+    title: 'GitHub Finder',
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <nav className='navbar bg-primary'>
-        <h1>Navbar</h1>
+        <h1>
+          <FontAwesomeIcon icon={this.props.icon} />
+          {this.props.title}
+        </h1>
       </nav>
     );
   }
