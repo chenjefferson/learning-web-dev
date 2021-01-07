3d9278fbd69fe640065be01f0dc07aee0d9c6c19
Jefferson William Chen <jeffwilliamchen@ieee.org>
2021-01-07 13:26:36 -0800

    added search UI

--- React/guided-projects/github-finder/src/components/layout/Navbar.js
+++ React/guided-projects/github-finder/src/components/layout/Navbar.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: 'GitHub Finder',
-  };
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <FontAwesomeIcon icon={icon} />
+        {title}
+      </h1>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-  };
-
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <FontAwesomeIcon icon={this.props.icon} />
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Navbar;
--- React/guided-projects/github-finder/src/components/users/UserItem.js
+++ React/guided-projects/github-finder/src/components/users/UserItem.js
@@ -1,37 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Navbar from '../layout/Navbar';
+import PropTypes from 'prop-types';
 
-export class UserItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: 1,
-      login: 'mojombo',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo',
-    };
-  }
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
 
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Navbar.defaultProps = {
+  title: 'GitHub Finder',
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default UserItem;
