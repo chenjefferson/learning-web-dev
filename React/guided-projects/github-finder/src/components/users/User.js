ae7b12db73a0dedd544c2c551920d81ffa2a8019
Jefferson William Chen <jeffwilliamchen@ieee.org>
2021-01-07 18:52:17 -0800

    added profile view

--- React/guided-projects/github-finder/src/components/users/User.js
+++ React/guided-projects/github-finder/src/components/users/User.js
@@ -2,6 +2,11 @@ import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTimesCircle,
+  faCheckSquare,
+} from '@fortawesome/free-solid-svg-icons';
 
 export class User extends Component {
   componentDidMount() {
@@ -22,6 +27,7 @@ export class User extends Component {
       bio,
       blog,
       login,
+      company,
       html_url,
       followers,
       following,
@@ -34,11 +40,71 @@ export class User extends Component {
 
     if (loading) return <Spinner />;
 
+    console.log(avatar_url);
     return (
       <Fragment>
         <Link to='/' className='btn btn-light'>
           Back to Search
         </Link>
+        Hireable:
+        {hireable ? (
+          <FontAwesomeIcon icon={faCheckSquare} className='text-success' />
+        ) : (
+          <FontAwesomeIcon icon={faTimesCircle} className='text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              alt=''
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit GitHub Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+
+          <div className='card text-center'>
+            <div className='badge badge-primary'>Followers: {followers}</div>
+            <div className='badge badge-success'>Following: {following}</div>
+            <div className='badge badge-danger'>Following: {public_repos}</div>
+            <div className='badge badge-dark'>Following: {public_gists}</div>
+          </div>
+        </div>
         {login}
       </Fragment>
     );
