1630cc569021677f5de85cebae2859b1a6782e9b
Jefferson William Chen <jeffwilliamchen@ieee.org>
2021-01-07 17:46:24 -0800

    added skeleton for User view

--- React/guided-projects/github-finder/src/App.js
+++ React/guided-projects/github-finder/src/App.js
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import axios from 'axios';
+import About from './components/pages/About';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   };
@@ -26,6 +29,16 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // get single GitHub user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ user: res.data, loading: false });
+  };
+
   clearUsers = () => this.setState({ users: [], loading: false });
 
   setAlert = (msg, type) => {
@@ -35,7 +48,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, user } = this.state;
 
     return (
       <Router>
@@ -60,7 +73,20 @@ class App extends Component {
                     </Fragment>
                   );
                 }}
-              ></Route>
+              />
+              <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
