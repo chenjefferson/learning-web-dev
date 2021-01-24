05f60aab2154a29f1c4980ca3a5cd60851c039c1
Jefferson William Chen <jeffwilliamchen@ieee.org>
2021-01-24 15:39:58 -0800

    added updated README and ASP exploration

---
+++ ASP/hello-razor-pages/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFramewokCore;
+
+namespace RazorPagesMovie.Data
+{
+    
+    public class RazorPagesMovieContext : DbContext
+    {
+      public RazorPagesMovieContext(
+        DbContextOptions<RazorPagesMovieContext> options
+      ) : base(options) {
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+      }
+    }
+}
